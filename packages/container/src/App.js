7acff50e87b4ae9bc80ca07a2bf21c1f42977d22
Anna Walasek <anna.walasek@ttpsc.com>
2025-08-14 20:48:18 +0200

    changes to make it work

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,6 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return <div>
     <h1> Hello! </h1>
+    <p> change to make it work</p>
     <hr />
     <MarketingApp/>
     </div>;
